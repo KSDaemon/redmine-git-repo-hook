223a50ef248c34de4057c0329f4400193ebda127
Konstantin Burkalev <KSDaemon@yandex.ru>
2014-02-01 00:32:40 +0400

    Added readme and place scripts

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 redmine-git-repo-hook
 =====================
 
-Scripts for updating redmine repo info on git post-receive hook
+Scripts for updating redmine repo info on git post-receive hook in gitlab (also works for any git repo).
+
+
+About
+-----
+
+This pair of scripts allows you to execute any shell commands on remote server when git post-receive hook is fired. The main goal is to notify redmine about changes in git repositories, which are connected to redmine projects. But you can use it for any other tasks :)
+
+Usage
+-----
+
+1. Place a "post-receive" shell script in your repo .git/hooks/ directory (or use symlink in case of more than one repo)
+2. Place config.ini and pr-hook.php in one place on redmine server accessible via http
+3. Edit "post-receive" shell script and change url to one, configured for step 2
+4. Edit config.ini to meet your need
+
+###### Some remarks:
+Git repositories on redmine host need to be already initialized.
---
+++ pr-hook.php
@@ -0,0 +1,61 @@
+<?php
+
+	$ini_array = parse_ini_file("config.ini", true);
+
+	if($ini_array === FALSE)
+		die('Cannot parse config file!');
+
+	if(!isset($ini_array['base']) || !isset($ini_array['commands'])) {
+		die('Not all required sections found in config file!');
+	}
+
+	try {
+
+		if($ini_array['base']['log_script'] && $ini_array['base']['log_file']) {
+			$logfile = fopen($ini_array['base']['log_file'], 'a');
+
+			if ($logfile === FALSE) {
+				throw new Exception("Cannot open log file: " . $ini_array['base']['log_file']);
+			}
+		}
+
+		if($data = json_decode(file_get_contents('php://input'), true)) {
+			$repo = $data['repository']['name'];
+
+			if (array_key_exists($repo, $ini_array)) {	// there is specific configuration for this repo
+				$cmd = 'cd ' . $ini_array[$repo]['git_repos_dir'] ;
+			}
+			else {	// use default configuration
+				$cmd = 'cd ' . $ini_array['base']['git_repos_dir'] . '/' . $repo;
+			}
+
+			array_unshift($ini_array['commands']['cmds'], $cmd);
+
+			$res_cmd = "/bin/sh -c 'exec 2>&1 && " . implode(" && ", $ini_array['commands']['cmds']) . "'";
+			$output = array();
+
+			exec($res_cmd, $output, $res);
+
+			if($ini_array['base']['log_script']) {
+				fwrite($logfile, date('Y-m-d H:i:s') . " CMD: " . $res_cmd . "\n");
+				fwrite($logfile, date('Y-m-d H:i:s') . " OUTPUT: " . implode("\n", $output) . "\n");
+			}
+
+		}
+		else {
+			throw new Exception("Cannot json_decode the HTTP_RAW_POST_DATA");
+		}
+	} catch (Exception $e) {
+
+		if($ini_array['base']['log_script']) {
+			fwrite($logfile, date('Y-m-d H:i:s') . ": " . $e . "\n");
+		}
+
+		error_log( sprintf( "%s: %s", date('Y-m-d H:i:s'), $e ) );
+	}
+
+	if ($logfile) {
+		fclose($logfile);
+	}
+
+?>
